34ddb20f2a904773aac3acdec0135b6d64ff0c64
Mariya <marisha_1793@mail.ru>
2022-12-13 00:51:51 +0700

    вторая задача

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13:Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет. Не 
+// использовать string[]
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+if (N > 99 && N < 1000)
+{
+   Console.WriteLine(N % 10); 
+}
+else if (N > 999)
+{
+    while (N > 999)
+    {
+        N = N / 10;
+    }
+    Console.WriteLine(N % 10);
+}
+else if (N < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+
