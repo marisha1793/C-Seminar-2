9395f7ed126c513559ec095967d70ad4f96694d4
Mariya <marisha_1793@mail.ru>
2022-12-12 22:33:53 +0700

    первая задача

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+int number2 = N / 10 % 10;
+if (N > 99 && N < 1000)
+{
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine("Это число не трехзначное, попробуй еще раз");
+}
